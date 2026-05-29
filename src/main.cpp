e24ad0b6fbf830ba8d235cdff68dbe416da27a6a
RoxVexat <normalcel976@gmail.com>
2026-05-29 21:24:23 +0300

    Кислинский 25-ИСТ-3

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,50 @@
 // Copyright 2022 NNTU-CS
 #include <iostream>
 #include "train.h"
+#include <cstdlib>
+#include <ctime>
 
-int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
+int main() 
+{
+   std::srand(static_cast<unsigned int>(time(nullptr)));
 
-  while (count--)
-    train.addCar(false);
+    std::cout << "n\tfalse\t\ttrue\t\trndom\n";
 
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
-  return 0;
+    for (int carriageCount = 2; carriageCount <= 100; carriageCount++) 
+    {
+        int operationsFalse;
+        int operationsTrue;
+        int operationsRandom;
+        {
+            Train train;
+            for (int i = 0; i < carriageCount; i++)
+                train.addCar(false);
+            train.getLength();
+            operationsFalse = train.getOpCount();
+        }
+
+        {
+            Train train;
+            for (int i = 0; i < carriageCount; i++)
+                train.addCar(true);
+            train.getLength();
+            operationsTrue = train.getOpCount();
+        }
+
+        {
+            Train train;
+            for (int i = 0; i < carriageCount; i++)
+                train.addCar(std::rand() % 2);
+            train.getLength();
+            operationsRandom = train.getOpCount();
+        }
+
+        std::cout << carriageCount << "\t"
+          << operationsFalse << "\t\t"
+          << operationsTrue << "\t\t"
+          << operationsRandom << "\n";
+    }
+
+    return 0;
+
 }
