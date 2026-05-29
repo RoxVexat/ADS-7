5f36ab198b33dc14317cfd4c3c73d30ec793ec3e
RoxVexat <normalcel976@gmail.com>
2026-05-29 23:06:45 +0300

    Кислинский 25-ИСТ-3

--- include/train.h
+++ include/train.h
@@ -4,18 +4,26 @@
 
 class Train {
  private:
-  struct Car {
-    bool light; // состояние лампочки
-    Car *next;
-    Car *prev;
-  };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Car *first; // точка входа в поезд (первый вагон)
-  Car* current;
+    struct Car {
+        bool light;
+        Car* next;
+        Car* prev;
+    };
+
+    int operations;   // количество выполненных переходов
+    Car* head;        // указатель на начало состава
+    Car* now;         // позиция текущего вагона
+
+    void resetCounter() { operations = 0; }
+    void moveForward();
+    void moveBackward();
+
  public:
-  Train();
-  void addCar(bool light); // добавить вагон с начальным состоянием лампочки
-  int getLength();          // вычислить длину поезда
-  int getOpCount();         // вернуть число переходов (из вагона в вагон)
+    Train();
+    ~Train();
+    void addCar(bool light);
+    int getLength();
+    int getOpCount() const;
 };
-#endif  // INCLUDE_TRAIN_H_
+
+#endif // INCLUDE_TRAIN_H_
